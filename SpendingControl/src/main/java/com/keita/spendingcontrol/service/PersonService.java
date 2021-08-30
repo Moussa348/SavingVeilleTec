0cf7b6c3c9af6922759cb255bcbc484204b6e67d
Moussa <Moussa.Keita2411@gmail.com>
2021-08-30 09:36:08 -0400

    adding PersonService --> createPerson()

--- SpendingControl/src/main/java/com/keita/spendingcontrol/service/PersonService.java
+++ SpendingControl/src/main/java/com/keita/spendingcontrol/service/PersonService.java
@@ -1,8 +1,13 @@
 package com.keita.spendingcontrol.service;
 
+import com.keita.spendingcontrol.model.entity.Person;
 import com.keita.spendingcontrol.repository.PersonRepository;
+import com.keita.spendingcontrol.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 @Service
 public class PersonService {
@@ -10,6 +15,13 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
-
+    public boolean createPerson(Person person) throws IOException {
+        if(!personRepository.existsByEmail(person.getEmail())){
+            person.setRegistrationDate(LocalDate.now());
+            person.setPicture(FileUtil.setDefaultProfilePicture());
+            return true;
+        }
+        return false;
+    }
 
 }
--- SpendingControl/src/test/java/com/keita/spendingcontrol/service/PersonServiceTest.java
+++ SpendingControl/src/test/java/com/keita/spendingcontrol/service/PersonServiceTest.java
@@ -1,13 +1,43 @@
 package com.keita.spendingcontrol.service;
 
+import com.keita.spendingcontrol.model.entity.Person;
 import com.keita.spendingcontrol.repository.PersonRepository;
+import com.keita.spendingcontrol.util.FileUtil;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
 
     @Mock
     PersonRepository personRepository;
+
+    @InjectMocks
+    PersonService personService;
+
+    @Test
+    void createPerson() throws IOException {
+        //ARRANGE
+        Person person1 = Person.builder().email("araa@gmail.com").build();
+        when(personRepository.existsByEmail(person1.getEmail())).thenReturn(false);
+
+        Person person2 = Person.builder().email("dasdads@gmail.com").build();
+        when(personRepository.existsByEmail(person2.getEmail())).thenReturn(true);
+
+        //ACT
+        boolean person1HasBeenCreated = personService.createPerson(person1);
+        boolean person2HasBeenCreated = personService.createPerson(person2);
+
+        //ASSERT
+        assertTrue(person1HasBeenCreated);
+        assertFalse(person2HasBeenCreated);
+    }
 }
