1218773c346432bd897c5bf34baad5cd669abe6a
Moussa <Moussa.Keita2411@gmail.com>
2021-08-30 08:09:15 -0400

    initialization of spring boot project

---
+++ SpendingControl/src/main/java/com/keita/spendingcontrol/SpendingControlApplication.java
@@ -0,0 +1,13 @@
+package com.keita.spendingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingControlApplication.class, args);
+    }
+
+}
---
+++ SpendingControl/src/test/java/com/keita/spendingcontrol/SpendingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keita.spendingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
