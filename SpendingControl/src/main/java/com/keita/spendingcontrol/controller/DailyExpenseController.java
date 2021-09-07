500ef8abffb1ea3cab0d900d2183c293f5a76d07
Moussa <Moussa.Keita2411@gmail.com>
2021-09-06 23:50:47 -0400

    adding @PreAuthorize with calling of authorizationService on DailyExpenseController + restesting

--- SpendingControl/src/main/java/com/keita/spendingcontrol/controller/DailyExpenseController.java
+++ SpendingControl/src/main/java/com/keita/spendingcontrol/controller/DailyExpenseController.java
@@ -5,6 +5,7 @@ import com.keita.spendingcontrol.model.dto.DailyExpenseDetail;
 import com.keita.spendingcontrol.service.DailyExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -17,11 +18,13 @@ public class DailyExpenseController {
     private DailyExpenseService dailyExpenseService;
 
     @PatchMapping("/addArticleToDailyExpense")
+    @PreAuthorize("@authorizationService.isConnected(#articleDetail.personId)")
     public void addArticleToDailyExpense(@RequestBody ArticleDetail articleDetail){
         dailyExpenseService.addArticleToDailyExpense(articleDetail);
     }
 
     @GetMapping("/getDailyExpenseByDateForPerson")
+    @PreAuthorize("@authorizationService.isConnected(#id)")
     public DailyExpenseDetail getDailyExpenseByDateForPerson(@RequestParam("id")Long id, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate date){
         return dailyExpenseService.getDailyExpenseByDateForPerson(id,date);
     }
--- SpendingControl/src/test/java/com/keita/spendingcontrol/controller/DailyExpenseControllerTest.java
+++ SpendingControl/src/test/java/com/keita/spendingcontrol/controller/DailyExpenseControllerTest.java
@@ -5,6 +5,7 @@ import com.keita.spendingcontrol.model.dto.ArticleDetail;
 import com.keita.spendingcontrol.model.entity.Article;
 import com.keita.spendingcontrol.model.entity.DailyExpense;
 import com.keita.spendingcontrol.model.entity.Person;
+import com.keita.spendingcontrol.security.JwtService;
 import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,32 +36,41 @@ public class DailyExpenseControllerTest {
     @Autowired
     ObjectMapper mapper;
 
+    @Autowired
+    JwtService jwtService;
+
     @Test
     void addArticleToDailyExpense () throws Exception{
         //ARRANGE
         DailyExpense dailyExpense = DailyExpense.builder().id(1L).person(Person.builder().id(1L).build()).build();
         ArticleDetail articleDetail = new ArticleDetail(Article.builder().dailyExpense(dailyExpense).build());
 
+        Person person1 = Person.builder().id(1L).roles("USER").build();
+        Long dailyExpenseId1 = 1L;
+        String token1 = "Bearer " + jwtService.generate(person1, dailyExpenseId1);
+
+        Person person2 = Person.builder().id(2L).roles("USER").build();
+        Long dailyExpenseId2 = 2L;
+        String token2 = "Bearer " + jwtService.generate(person2, dailyExpenseId2);
+
         //ACT
         MvcResult mvcResult1 = mockMvc.perform(MockMvcRequestBuilders.patch("/dailyExpense/addArticleToDailyExpense")
-                //.header(HttpHeaders.AUTHORIZATION, token1)
+                .header(HttpHeaders.AUTHORIZATION, token1)
                 .content(mapper.writeValueAsString(articleDetail))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
 
-        /*
         MvcResult mvcResult2 = mockMvc.perform(MockMvcRequestBuilders.patch("/dailyExpense/addArticleToDailyExpense")
-                //.header(HttpHeaders.AUTHORIZATION, token2)
+                .header(HttpHeaders.AUTHORIZATION, token2)
                 .content(mapper.writeValueAsString(articleDetail))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andReturn();
-         */
+                .andExpect(status().isForbidden()).andReturn();
 
         //ASSERT
         assertEquals(MockHttpServletResponse.SC_OK, mvcResult1.getResponse().getStatus());
-        //assertEquals(MockHttpServletResponse.SC_OK, mvcResult2.getResponse().getStatus());
+        assertEquals(MockHttpServletResponse.SC_FORBIDDEN, mvcResult2.getResponse().getStatus());
     }
 
     @Test
@@ -69,28 +79,33 @@ public class DailyExpenseControllerTest {
         Long id = 1L;
         LocalDate date = LocalDate.now();
 
+        Person person1 = Person.builder().id(1L).roles("USER").build();
+        Long dailyExpenseId1 = 1L;
+        String token1 = "Bearer " + jwtService.generate(person1, dailyExpenseId1);
+
+        Person person2 = Person.builder().id(2L).roles("USER").build();
+        Long dailyExpenseId2 = 2L;
+        String token2 = "Bearer " + jwtService.generate(person2, dailyExpenseId2);
+
         //ACT
         MvcResult mvcResult1 = mockMvc.perform(MockMvcRequestBuilders.get("/dailyExpense/getDailyExpenseByDateForPerson")
-                //.header(HttpHeaders.AUTHORIZATION, token1)
+                .header(HttpHeaders.AUTHORIZATION, token1)
                 .param("id",id.toString())
                 .param("date",date.toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
 
-        /*
         MvcResult mvcResult2 = mockMvc.perform(MockMvcRequestBuilders.get("/dailyExpense/getDailyExpenseByDateForPerson")
-                //.header(HttpHeaders.AUTHORIZATION, token2)
+                .header(HttpHeaders.AUTHORIZATION, token2)
                 .param("id",id.toString())
                 .param("date",date.toString())
-                .content(mapper.writeValueAsString(articleDetail))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andReturn();
-         */
+                .andExpect(status().isForbidden()).andReturn();
 
         //ASSERT
         assertEquals(MockHttpServletResponse.SC_OK, mvcResult1.getResponse().getStatus());
-        //assertEquals(MockHttpServletResponse.SC_OK, mvcResult2.getResponse().getStatus());
+        assertEquals(MockHttpServletResponse.SC_FORBIDDEN, mvcResult2.getResponse().getStatus());
     }
 }
