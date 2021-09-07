e5e75afc4759fc2b39f89fed3305f94b17e432eb
Moussa <Moussa.Keita2411@gmail.com>
2021-09-07 00:53:25 -0400

    adding a constant.ts file to use it in all the project

--- SpendingControlAng/src/app/app.module.ts
+++ SpendingControlAng/src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config:{
+        tokenGetter: () => {
+          return sessionStorage.getItem(STORAGE_KEY);
+        },
+        allowedDomains:ALLOWED_URLS
+      },
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
