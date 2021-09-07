23d8ec586baff4ccaf669b199611aec3aba8df74
Moussa <Moussa.Keita2411@gmail.com>
2021-09-07 12:28:49 -0400

    adding animation on icon in WelcomeComponent

--- SpendingControlAng/src/app/component/welcome/welcome.component.ts
+++ SpendingControlAng/src/app/component/welcome/welcome.component.ts
@@ -1,15 +1,33 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(2160deg)' })),
+      transition('rotated => default', animate('400ms ease-out')),
+      transition('default => rotated', animate('400ms ease-in')),
+    ]),
+  ],
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  state: string = 'default';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  rotate() {this.state = (this.state === 'default' ? 'rotated' : 'default');}
 }
