b6e83a135bc8b066cd763c389656a5eb5e91fb79
Moussa <Moussa.Keita2411@gmail.com>
2021-09-06 23:17:33 -0400

    adding JwtService --> verify() to be able to verify a token on every request

--- SpendingControl/src/test/java/com/keita/spendingcontrol/security/JwtServiceTest.java
+++ SpendingControl/src/test/java/com/keita/spendingcontrol/security/JwtServiceTest.java
@@ -1,11 +1,19 @@
 package com.keita.spendingcontrol.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.keita.spendingcontrol.model.entity.Person;
 import jdk.jfr.Percentage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
+import static com.keita.spendingcontrol.security.JwtService.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JwtServiceTest {
 
@@ -26,6 +34,27 @@ public class JwtServiceTest {
         String token = jwtService.generate(person,dailyExpenseId);
 
         //ASSERT
-        assertEquals(person.getId(), jwtService.getJwtVerifier().verify(token).getClaim(JwtService.PERSON_ID_CLAIM).asLong());
+        assertEquals(person.getId(), jwtService.getJwtVerifier().verify(token).getClaim(PERSON_ID_CLAIM).asLong());
+    }
+
+    @Test
+    void verify(){
+        //ARRANGE
+        Person person = Person.builder().id(1L).roles("USER").build();
+        Long dailyExpenseId = 1L;
+        String token1 = "Bearer " + JWT.create().withSubject(person.getId().toString())
+                .withClaim(PERSON_ID_CLAIM, person.getId())
+                .withClaim(PERSON_ROLE_CLAIM,person.getRoles())
+                .withClaim(PERSON_DAILY_EXPENSE_ID_CLAIM, dailyExpenseId.toString())
+                .sign(jwtService.getAlgorithm());
+        String token2 =  "asdadsasdasdads";
+
+        //ACT
+        DecodedJWT decodedJWT = jwtService.verify(token1);
+
+        //ASSERT
+        assertEquals(person.getId(),decodedJWT.getClaim(PERSON_ID_CLAIM).asLong());
+        assertThrows(JWTDecodeException.class,() -> jwtService.verify(token2));
+        assertThrows(JWTVerificationException.class,()->jwtService.verify(token2));
     }
 }
