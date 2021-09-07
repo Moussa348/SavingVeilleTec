cd98bb3c6b9ac36a915f7069dd916cbe5c593292
Moussa <Moussa.Keita2411@gmail.com>
2021-09-07 01:27:54 -0400

    adding authGuardService to protect routes in front-end

---
+++ SpendingControlAng/src/app/service/auth-guard.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService {
+
+  constructor(private router : Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot) {
+    const url = route.url.join('');
+    
+    if(!this.isLoggedIn() && (url != 'home')){
+      this.router.navigate(['/login']);
+      return false;
+    }
+    if(this.isLoggedIn() && url == 'registration'){
+      this.router.navigate(['/home'])
+      return false;
+    }
+    return true;
+  }
+  
+  isLoggedIn() {
+    return sessionStorage.getItem('token')!=null;
+  }
+
+  login(token:string){
+    sessionStorage.setItem('token',token);
+  }
+
+  logout(){
+    sessionStorage.clear();
+  }
+}
