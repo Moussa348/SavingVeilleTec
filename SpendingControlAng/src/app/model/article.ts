1b124ce3d343f48909bbe8f259bc44f6503772f9
Moussa <Moussa.Keita2411@gmail.com>
2021-09-09 12:30:38 -0400

    adding article model

---
+++ SpendingControlAng/src/app/model/article.ts
@@ -0,0 +1,9 @@
+export class Article {
+    id:number;
+    personId:number;
+    time:string;
+    name:string;
+    qty:number;
+    price:number;
+    degreeOfUseFullness:string;
+}
--- SpendingControlAng/src/app/model/daily-expense.ts
+++ SpendingControlAng/src/app/model/daily-expense.ts
@@ -1,7 +1,9 @@
+import { Article } from "./article";
 
 export class DailyExpense {
     id:number;
     date:string;
     total:number;
+    articles:Article[] = new Array();
     mapArticlesUseFullness:Map<string,number> = new Map();
 }
